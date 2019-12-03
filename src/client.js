f85c0ad662441a97f023d4dd48ed2e2c0612d003
AnarchonGE <33235178+AnarchonGE@users.noreply.github.com>
2019-12-03 00:12:00 -0500

    Update client.js

--- src/client.js
+++ src/client.js
@@ -6,7 +6,9 @@ sapper.start({
 });
 
 if (process.env.SENTRY_DSN) {
-  Sentry.init({ dsn: 'https://19993e43262e4461be99e9e009a4d3af@sentry.io/1832504' });
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+  });
 }
 
 // Sentry.captureException(new Error("Something broke"));
