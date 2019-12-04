3d8cc887c0e2afd43f0b193dc87095b7fe372bd6
AnarchonGE <33235178+AnarchonGE@users.noreply.github.com>
2019-12-03 23:45:10 -0500

    Create sentrytest.svelte

---
+++ src/routes/sappit/sentrytest.svelte
@@ -0,0 +1,10 @@
+<p>a test error should have been sent</p>
+
+<script>
+import { onMount } from 'svelte';
+import * as Sentry from '@sentry/browser';
+
+onMount(() => {
+  Sentry.captureException(new Error("test error"));
+});
+</script>
