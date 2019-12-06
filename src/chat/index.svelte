0e6ed91f53b924431437cc0cdd6023c05b701c9f
AnarchonGE <33235178+AnarchonGE@users.noreply.github.com>
2019-12-06 09:39:31 -0500

    Create index.svelte

---
+++ src/chat/index.svelte
@@ -0,0 +1,5 @@
+<h2>chat</h2>
+
+<a href="https://www.reddit.com/chat" rel="nofollow" target="_blank">reddit.com/chat</a>
+
+<p>Here is a link to the standalone reddit chat html5 app. If it's possible, chat might get implemented here one day</p>
