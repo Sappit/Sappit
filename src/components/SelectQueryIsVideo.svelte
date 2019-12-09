b1dd218a1d82c7cad8e567d757b9b03943f65fb8
AnarchonGE <33235178+AnarchonGE@users.noreply.github.com>
2019-12-09 11:55:15 -0500

    Create SelectQueryIsVideo.svelte

---
+++ src/components/SelectQueryIsVideo.svelte
@@ -0,0 +1,23 @@
+lines (21 sloc) 597 Bytes
+<select
+  class="form-control r-select"
+  name="is_video"
+  {value}
+  on:change={(event) => $is_video = event.target.value}
+>
+  {#each selectOptions as {value, text} (value)}
+    <option {value}>{text}</option>
+  {/each}
+</select>
+
+<script>
+import queryStore from '~/store/query';
+const { is_video } = queryStore;
+const selectOptions = Object.freeze([
+  { value: null, text: 'Video+Non-Video Posts' },
+  { value: '1', text: 'Video Posts Only' },
+  { value: '0', text: 'Non-Video Posts Only' },
+]);
+let defaultValue = selectOptions[0].value;
+$: value = $is_video ? parseInt($is_video) : defaultValue;
+</script>
