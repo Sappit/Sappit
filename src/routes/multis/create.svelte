d0e76888ccabea591abaad33849beb6029013cf8
anonymous <>
2019-12-10 21:29:06 +0000

    WIP

--- src/routes/multis/create.svelte
+++ src/routes/multis/create.svelte
@@ -26,57 +26,65 @@
     </div>
   </div>
 {/if}
+{#if createdMulti}
+  <ShowSource value={createdMulti} />
+{/if}
 
 <script>
 import ErrorAlert from '~/components/ErrorAlert';
+import ShowSource from '~/components/ShowSource';
 import reddit from '~/lib/reddit';
+import undata from '~/lib/undata';
 import middlewareAuth from '~/lib/middleware/auth';
 import { username } from '~/store/auth';
 
 let saving = false
 let multipath = ''
 let error = null
-let saved = null
+let createdMulti = null
+/*
 
+kind	LabeledMulti
+can_edit	true
+display_name	fgdgdfgdf
+name	fgdgdfgdf
+description_html	
+num_subscribers	0
+copied_from	null
+icon_url	https://www.redditstatic.com/custom_feeds/custom_feed_default_1.png
+subreddits	[]
+created_utc	1576012141
+visibility	private
+created	1576040941
+over_18	false
+path	/user/${username_lc}/m/${multipath}/
+owner	Username
+key_color	null
+is_subscriber	false
+owner_id	t2_00000000
+description_md	
+is_favorited	false
+ */
+
+// docs are VERY ???? on this one
 async function save () {
   try {
     error = null
     saving = true
 
-    //const path = `/api/multi/${multipath}`;
     const path = `/user/${$username}/m/${multipath}`;
 
     const response = await reddit.post(`/api/multi/${path}`, {
-      /*
-      model: {
-        "description_md": raw markdown text,
-        "display_name": a string no longer than 50 characters,
-        "icon_img": one of (`png`, `jpg`, `jpeg`),
-        "key_color": a 6-digit rgb hex color, e.g. `#AABBCC`,
-        "subreddits": [
-          {
-            "name": subreddit name,
-          },
-          ...
-        ],
-        "visibility": one of (`private`, `public`, `hidden`),
-      },//*/
-//      model: JSON.stringify({
-//        display_name: multipath,
-//        visibility: 'private',
-//        multipath,
-//      }),
-//      multipath,
-        // what the website actually does
-        // model:{"path":"/user/${username}/m/${multipath}","visibility":"private","subreddits":[]}
         model: JSON.stringify({
-//          path: `/user/${$username}/m/${multipath}`,
           path,
+          display_name: multipath,
           visibility: 'private',
           subreddits: [],
         }),
     })
-    
+
+    createdMulti = undata(response.data);    
+
     multipath = ''
   } catch (err) {
     console.error(err)
