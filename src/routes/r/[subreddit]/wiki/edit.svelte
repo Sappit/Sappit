6e7fa44437b6510f65d2fb129dc680ce91005bc7
anonymous <>
2020-01-27 17:10:17 +0000

    WIP wiki config

---
+++ src/routes/r/[subreddit]/wiki/edit.svelte
@@ -0,0 +1,38 @@
+<h2>Wiki Edit</h2>
+
+{#await promise}
+  <Loading/>
+{:then collection}
+  <ShowSource :value="collection" />
+{:catch error}
+  <ErrorAlert value={error} />
+{/await}
+
+<script>
+import ErrorAlert from '~/components/ErrorAlert';
+import ShowSource from '~/components/ShowSource';
+import Loading from '~/components/Loading';
+import AddContributorForm from '~/components/AddContributorForm';
+import ApprovedUserList from '~/components/ApprovedUserList';
+import RedditPagination from '~/components/RedditPagination';
+import redditItems from '~/lib/redditItems';
+import middlewareAuth from '~/lib/middleware/auth';
+// import validatePropSubreddit from '~/lib/validateProp/subreddit';
+import { stores } from '@sapper/app'
+const { page } = stores();
+
+// props
+// export let subreddit;
+
+let promise = null;
+
+// $: validatePropSubreddit(subreddit);
+
+middlewareAuth()
+$: subname = $page.params.subreddit;
+$: if (subname) promise = redditItems({
+  path: `/r/${subname}/wiki/edit`,
+  query: $page.query,
+});
+
+</script>
